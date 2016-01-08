fc5b413a4390244fd2afd31ee958612657141cce
Radu Surducan <radu.surducan@autodesk.com>
2016-01-08 07:56:26 -0500

    Starting from existing Thinkful repository with some small changes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,38 @@
-angular.module('calendarDemoApp', []);
+angular.module("calendarApp", [])
+  .controller('CalendarCtrl', function($scope) {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth();
 
-// your controller and directive code go here
+    $scope.date = {
+      year : year,
+      month : month
+    };
+
+    $scope.isCurrentMonth = function(date) {
+      return date.getMonth() == $scope.date.month;
+    }
+
+    $scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    $scope.years = [];
+    for(var i = year - 20; i <= year + 20; i++) {
+      $scope.years.push(i);
+    }
+
+    $scope.$watchCollection('date', function(date) {
+      $scope.currentDate = new Date(date.year, date.month, 1);
+    });
+  })
+  .directive('myCalendar', function() {
+    return {
+      transclude : true,
+      templateUrl : './calendar.html',
+      controller : ['$scope', '$attrs', function($scope, $attrs) {
+        $scope.$watch($attrs.myCalendar, function(date) {
+          if(!date) return;
+          $scope.days = CalendarRange.getMonthlyRange(date).days;
+        });
+      }]
+    }
+  });
